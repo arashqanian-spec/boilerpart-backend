0885cb0743da62d82f74fc672219cfa48648fc78
arashqanian-spec <arash.qanian@gmail.com>
2026-08-16 15:41:54 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -32,3 +32,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
  console.log("API running on "+PORT);
 });
+
+app.post('/admin/login', (req,res)=>{
+ const {username,password}=req.body;
+
+ if(username==='arash' && password==='arash1373'){
+   return res.json({
+     success:true,
+     token:'boilerpart-admin-token'
+   });
+ }
+
+ res.status(401).json({
+   success:false,
+   message:'Invalid credentials'
+ });
+});
