6b676c5c9102272b0f016a50b95f980991400a0d
arashqanian-spec <arash.qanian@gmail.com>
2026-08-16 18:25:56 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const API='https://boilerpart-backend-production.up.railway.app/api/products';
+const API="https://boilerpart-backend-production.up.railway.app";
 
-async function load(){
-try{
-const r=await fetch(API);
-const data=await r.json();
-document.getElementById('products-list').innerHTML=data.map(p=>
-`<div class="card"><h3>${p.name}</h3><p>${p.price||''}</p></div>`
-).join('');
-}catch(e){
-document.getElementById('products-list').innerHTML='در انتظار اتصال API';
+async function login(){
+ const username=document.getElementById("username").value;
+ const password=document.getElementById("password").value;
+
+ const r=await fetch(API+"/admin/login",{
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json"
+  },
+  body:JSON.stringify({
+   username,
+   password
+  })
+ });
+
+ const data=await r.json();
+
+ if(data.success){
+  localStorage.setItem("token",data.token);
+  window.location.href="admin.html";
+ }
+ else{
+  document.getElementById("msg").innerText="اطلاعات اشتباه";
+ }
 }
-}
-load();
